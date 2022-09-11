e30b84e9650e0352f58d1368d916c46643d34176
Ivan Chernov <vanyaz158@gmail.com>
2022-09-11 14:41:54 +0300

    Remove unused module

--- lib/smart_validator/validation_state.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module SmartValidator
-  module ValidationState
-  end
-end
-
-require_relative "validation_state/errors"
-require_relative "validation_state/manager"
